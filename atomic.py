68bc7630faaeaf1c9544155ceba2f081c5990e3a
ShadyKid <35014054+drnerd47@users.noreply.github.com>
2023-01-08 22:24:06 +0530

    minor fixes

--- atomic.py
+++ atomic.py
@@ -12,7 +12,7 @@ def ReadExcelFile(filename):
 def GetCompletions(api_key, prompt, question, engine, tokens):
     openai.organization = "org-S3POWFAdvhzHFxU7FLRAMh4g"
     openai.api_key = api_key
-    completion = openai.Completion.create(engine=engine, max_tokens=tokens, prompt= str(prompt) + " | " + question)
+    completion = openai.Completion.create(engine=engine, max_tokens=tokens, prompt= str(prompt) + " || " + question)
     return completion.choices[0].text
 
 def GetResponseFromOpenAI(api_key, question, engine, tokens, filename):
@@ -22,14 +22,16 @@ def GetResponseFromOpenAI(api_key, question, engine, tokens, filename):
 
     for m in range(len(messages)):
         print("On message number - ", m)
+        print("\n")
         prompt = messages.iloc[m]
         print("PROMPT IS - ", prompt)
+        print("\n")
         response = GetCompletions(api_key, prompt, question, engine, tokens)
         print("RESPONSE IS - ",response)
-        print("\n")
+        print("\n\n")
         response_list.append(response)
         time.sleep(5)
 
-    responses = pd.concat(response_list)
-    responses.to_csv("Result of - " + filename)
+    responses = pd.DataFrame(response_list)
+    responses.to_csv('/content/drive/MyDrive/' + "Results.csv")
     
