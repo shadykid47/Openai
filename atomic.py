1a806e3085d5837bc8e4b9f2eb234d90491c027c
ShadyKid <35014054+drnerd47@users.noreply.github.com>
2023-01-08 20:44:57 +0530

    removed api_key from code

--- atomic.py
+++ atomic.py
@@ -9,21 +9,20 @@ def ReadExcelFile(filename):
         df = pd.read_csv(filename)
     return df
 
-def GetCompletions(prompt, question, engine, tokens):
-    api_key = "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"
+def GetCompletions(api_key, prompt, question, engine, tokens):
     openai.organization = "org-S3POWFAdvhzHFxU7FLRAMh4g"
     openai.api_key = api_key
     completion = openai.Completion.create(engine=engine, max_tokens=tokens, prompt= str(prompt) + " | " + question)
     return completion
 
-def GetResponseFromOpenAI(question, engine, tokens, filename):
+def GetResponseFromOpenAI(api_key, question, engine, tokens, filename):
     filepath = '/content/drive/MyDrive/' + filename
     messages = ReadExcelFile(filepath)
     response_list = []
 
     for m in range(len(messages)):
         prompt = messages.iloc[m]
-        response = GetCompletions(prompt, question, engine, tokens)
+        response = GetCompletions(api_key, prompt, question, engine, tokens)
         response_list.append(response)
         time.sleep(2)
 
